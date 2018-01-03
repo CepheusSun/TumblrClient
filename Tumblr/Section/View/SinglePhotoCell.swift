31ecbb4f1f76d69c5e05e9576cd894090fa3f0e8
CepheusSun <cd_sunyang@163.com>
2018-01-03 15:24:38 +0800

    update

---
+++ Tumblr/Section/View/SinglePhotoCell.swift
@@ -0,0 +1,18 @@
+//
+//  SinglePhotoCell.swift
+//  Tumblr
+//
+//  Created by sunny on 2018/1/3.
+//  Copyright © 2018年 CepheusSun. All rights reserved.
+//
+
+import UIKit
+
+class SinglePhotoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
